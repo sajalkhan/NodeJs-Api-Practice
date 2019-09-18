75dd0ab4a4a37d606cae12c35d616ec685b1c8b1
sajalkhan <sajalhossen@yahoo.com>
2019-09-19 01:06:05 +0600

    Handling post Request

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
+app.use(express.json()); // include this middleware to put body data 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
 
 app.get('/api/v1/tours', (req,res)=>{
@@ -14,6 +15,27 @@ app.get('/api/v1/tours', (req,res)=>{
         data:{
             tours
         }
+    });
+});
+
+app.post('/api/v1/tours', (req,res)=>{
+    
+    // express doesn't put body data to request that's why we need middleware
+    // console.log(req.body);
+
+    const newId = tours[tours.length -1].id + 1; // get last id and add +1 
+    const newTour = Object.assign({id: newId}, req.body); // object.assign() allows us to create a new object with our existing object
+
+    tours.push(newTour); // add newTour object to tours array
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`,JSON.stringify(tours), err=>{
+
+        res.status(201).json({
+            message:'success',
+            data:{
+                tour: newTour
+            }
+
+        });
     });
 });
 
