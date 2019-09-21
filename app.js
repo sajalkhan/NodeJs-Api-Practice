cb8b20edcec7a9a950fc09010080a5ff0f9c78c1
sajalkhan <sajalhossen@yahoo.com>
2019-09-21 20:45:34 +0600

    Implement API get/post/patch/delete method

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const port = 8080;
 app.use(express.json()); // include this middleware to put body data 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
 
+//read all object data
 app.get('/api/v1/tours', (req,res)=>{
 
     res.status(200).json({
@@ -18,6 +19,32 @@ app.get('/api/v1/tours', (req,res)=>{
     });
 });
 
+//read specific data
+app.get('/api/v1/tours/:id', (req, res) => {  // responding to url parameters
+
+    //console.log(req.params); // req.params are store all the variable store inside the url
+
+    const id = req.params.id * 1; // convert string to number & javascript will convert it when we multipy it by a number;
+    const tour = tours.find(el => el.id === id); // javascrit find that specific element from an array
+
+    // if(id>tours.length)
+    if(!tour)
+    {
+        res.status(404).json({
+            status:'fail',
+            message:'Invalid id'
+        });
+    } 
+
+    res.status(200).json({
+        message: 'success',
+        data: {
+            tours: tour
+        }
+    });
+});
+
+//create data
 app.post('/api/v1/tours', (req,res)=>{
     
     // express doesn't put body data to request that's why we need middleware
@@ -33,6 +60,69 @@ app.post('/api/v1/tours', (req,res)=>{
             message:'success',
             data:{
                 tour: newTour
+            }
+
+        });
+    });
+});
+
+//update data
+app.patch('/api/v1/tours/:id', (req,res)=>{
+
+    if(req.params.id * 1 > tours.length )
+    {
+        res.status(404).json({
+            status:'fail!',
+            message:'Invalid id'
+        });
+    }
+
+    const id = req.params.id * 1;
+    const tour = tours.find(el => el.id === id);
+    const reqObj = req.body;
+
+    for(var x in tour)  // update tour object
+    {
+        if(x!=='id'){
+            tour[x] = reqObj[x];
+        }
+    }
+
+    tours.splice(id,1,tour); // remove that index and replace with other value
+
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`,JSON.stringify(tours), err=>{
+
+        res.status(201).json({
+            message:'Updated successfuly!',
+            data:{
+                tour: tour
+            }
+
+        });
+    });
+
+});
+
+//delete data
+app.delete('/api/v1/tours/:id' , (req,res) =>{
+
+    if(req.body.id * 1> tours.length)
+    {
+        res.status(404).json({
+            status: 'Fail',
+            message: 'Invalid Id!'
+        });
+    }
+
+    const id = req.params.id * 1;
+    tours.splice(id,1);
+
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`,JSON.stringify(tours), err=>{
+
+        res.status(201).json({
+            message:'Updated successfuly!',
+            data:{
+                tour: tours
             }
 
         });
