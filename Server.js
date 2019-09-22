04c56b72206d47d8f6d5ec4fd983e32364e08459
sajalkhan <sajalhossen@yahoo.com>
2019-09-22 17:16:18 +0600

    Connect our Hosted Database with  our express app

--- Server.js
+++ Server.js
@@ -1,9 +1,20 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
 dotenv.config({path: './config.env'}); // here will pass an object to env so it will understand where the configuration file is located
 // console.log(app.get('env'));
 // console.log(process.env);
+
+const DB = process.env.DATABASE.replace('<PASSWORD>',process.env.DATABASE_PASSWORD);
+
+mongoose.connect(DB,{
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology:true,
+    useFindAndModify:false
+})
+.then(()=> console.log('Db Connection Successful!') );
 
 const port = process.env.PORT || 8080;
 
